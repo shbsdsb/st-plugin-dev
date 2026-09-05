47579afd2eb06efef54fb69ae1060153ed844ada
shb <shb1348331639@163.com>
2026-09-05 23:20:13 +0800

    chore(logger-plugin): 包脚手架(Task 1)

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Context } from 'cordis'
+
+export const name = 'logger-plugin'
+
+export function apply(ctx: Context) {
+  // Task 6 装配 observer/format/writer
+  void ctx
+}
+
+export default apply
